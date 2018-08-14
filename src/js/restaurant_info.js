3bd1dd7d60befab790a506ec0ce74e8a0631a857
vschau <vanessaschau@gmail.com>
2018-08-13 17:46:53 -0700

    Fix readme

--- src/js/restaurant_info.js
+++ src/js/restaurant_info.js
@@ -32,7 +32,7 @@ let initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1IjoiZ25lc3NhMTQiLCJhIjoiY2ppejB0Z2c0MDI1bzNwbW1vaGZqdWxybyJ9.J8ct-vep4sreAdmjXxVIAg',
+        mapboxToken: 'add your mapbox token here',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
